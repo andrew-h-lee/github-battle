1c2744c56d2c50a065ab31d4f35afd3c981d7c54
dark419 <mr.ahlee@gmail.com>
2016-04-25 18:39:20 +1000

    New Changes

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,19 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+  render: function () {
+    return (
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" >
+        <h1>HEADER TEXT</h1>
+        <div className="col-sm-12">
+          <form>
+
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = PromptContainer;
