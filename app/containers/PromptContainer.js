ecf128a82bf5e91fd5dabd0dc4bf9925d2f799fb
andrew-h-lee <mr.ahlee@gmail.com>
2016-06-17 00:20:42 +1000

    Add Files from Video 5: Prompt & Stateless Functions

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,16 +1,46 @@
 var React = require('react');
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+  handleUpdateUser: function (e) {
+    this.setState ({
+      username: e.target.value
+    })
+  },
+  handleSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState ({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
   render: function () {
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" >
-        <h1>HEADER TEXT</h1>
-        <div className="col-sm-12">
-          <form>
-
-          </form>
-        </div>
-      </div>
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     );
   }
 
