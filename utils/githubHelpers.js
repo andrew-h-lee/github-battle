31a7f26706e698ea2405cf6e1806f3d68ea04a7a
andrew-h-lee <mr.ahlee@gmail.com>
2016-06-21 00:03:13 +1000

    Add Github Helpers

---
+++ utils/githubHelpers.js
@@ -0,0 +1,13 @@
+var axios = require('axios');
+
+function getUserInfo (username) {
+
+}
+
+var helpers = {
+  getPlayersInfo: function () {
+    // fetch data form gtihub
+  }
+};
+
+module.exports = helpers;
